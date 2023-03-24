4230ae65aabd6a0d70ed718333915352f521f736
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-03-23 22:31:40 -0700

    Completed Lab; modified formatting to work with CodeGrade

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
 const returnLastTwoDrivers = (drivers) => drivers.slice(2);
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-createFareMultiplier = (multiplierValue) => fareValue => fareValue * multiplierValue;
-const fareDoubler = (fare) => createFareMultiplier(2)(fare);
-const fareTripler = (fare) => createFareMultiplier(3)(fare);
+function createFareMultiplier (multiplierValue) {
+  return fareValue => fareValue * multiplierValue;
+}
+const fareDoubler = function(fare) {
+  return createFareMultiplier(2)(fare);
+}
+const fareTripler = function(fare) {
+  return createFareMultiplier(3)(fare);
+}
 
-selectDifferentDrivers = (drivers, selectionFunction) => {
+function selectDifferentDrivers(drivers, selectionFunction) {
   if (selectionFunction === returnFirstTwoDrivers) {
     return returnFirstTwoDrivers(drivers);
   } 
