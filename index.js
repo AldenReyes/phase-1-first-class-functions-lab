7afe749d5b9d181ea8ef6a01cb8f0f454f867fc0
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-03-23 22:23:41 -0700

    Completed Lab; examined the use of first class functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
+const returnLastTwoDrivers = (drivers) => drivers.slice(2);
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+createFareMultiplier = (multiplierValue) => fareValue => fareValue * multiplierValue;
+const fareDoubler = (fare) => createFareMultiplier(2)(fare);
+const fareTripler = (fare) => createFareMultiplier(3)(fare);
+
+selectDifferentDrivers = (drivers, selectionFunction) => {
+  if (selectionFunction === returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+  } 
+  else if (selectionFunction === returnLastTwoDrivers) {
+    return returnLastTwoDrivers(drivers);
+  }
+  else console.log("error")
+}
+  
